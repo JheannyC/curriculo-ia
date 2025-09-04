5ad94b4172fd1a6bfcd407ab7ce94a047f44d5db
programadorafull <progrmadorafull@gmail.com>
2025-09-04 19:44:26 -0300

    Refatorando PreviewSection para aceitar children

--- src/components/Layout/PreviewSection.tsx
+++ src/components/Layout/PreviewSection.tsx
@@ -1,7 +1,10 @@
-export default function PreviewSection({}: {}) {
+
+export default function PreviewSection({ children }: Props) {
   return (
     <aside className="bg-white border rounded-xl shadow flex flex-col overflow-hidden">
-      
+      <div className="flex-1 min-h-0 overflow-y-auto">
+        {children}
+      </div>
     </aside>
   );
 }
